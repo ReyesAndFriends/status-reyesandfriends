4665d770177c92bf40e4e4d9b81e1b934fb926a3
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-01 15:40:15 -0300

    feat: se inicia un proyecto Laravel 12 + Sanctum como base del código incluyendo migraciones y todo lo necesario.

---
+++ status-reyesandfriends/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health', function (Request $request) {
+    return response()->json(['message' => 'Everything is OK! for now...']);
+});
